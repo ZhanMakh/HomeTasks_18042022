5942a84489a8a68bcca9d8b6b6e9d122a908895f
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-04-20 20:59:26 +0600

    Добавлено решение задачи № 15

---
+++ Task15/Program.cs
@@ -0,0 +1,7 @@
+﻿// Домашнее задание.Решение задачи № 15.
+Console.WriteLine("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 | day == 7)
+{
+    Console.WriteLine("Этот день недели является выходным!");
+} else Console.WriteLine("Это будний день");
