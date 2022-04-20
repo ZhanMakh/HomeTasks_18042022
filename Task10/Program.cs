658a948d678fc95ba1dea266dddbc80dfae9f71e
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-04-20 19:28:20 +0600

    Добавлено решение задачи № 10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Домашнее задание. Задача № 10
+Console.WriteLine("Введите трехзначное число:");
+int num = int.Parse(Console.ReadLine());
+int num1 = num%100;
+int num2 = num1/10;
+Console.WriteLine($"Вторая цифра в данном числе будет {num2}");
