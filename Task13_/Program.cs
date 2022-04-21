e91a4138e00fa007a8d00de8437f92b0630582a9
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-04-21 15:48:29 +0600

    Внесены корректировки в решение задачи № 13

---
+++ Task13_/Program.cs
@@ -0,0 +1,19 @@
+﻿// Домашнее задание. Решение задачи № 13.
+Console.WriteLine("Введите любое целое число до 10 000: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (num<1000)
+{
+    num = num%10;
+    Console.WriteLine($"Третья цифра в данном числе равна {num}");
+} else if(num<10000)
+{
+    num = num/10;
+    num = num%10;
+    Console.WriteLine($"Третья цифра в данном числе равна {num}");
+} else Console.WriteLine("Это число больше 10000");
+    
