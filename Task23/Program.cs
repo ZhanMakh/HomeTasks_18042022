f8f32dbec810b051c5d0aef2a1c45516d4621568
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-04-24 22:14:36 +0600

    Добавлено решение задачи № 23

---
+++ Task23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Домашнее задание. Решение задачи № 23
+Console.Write("Введите любое число: ");
+int num = int.Parse(Console.ReadLine());
+int index = 1;
+while (index<=num)
+{
+    Console.Write(Math.Pow(index,3)+" ");
+    index++;
+}
