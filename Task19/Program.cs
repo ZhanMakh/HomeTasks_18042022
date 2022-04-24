8a19ab98f8e86542669ef471c6c98e3a95f91ffb
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-04-24 21:46:06 +0600

    Добавлено решение задачи №19

---
+++ Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание. Решение задачи № 19
+Console.Write("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int x1 = num/10000;
+int x21 = num/1000;
+int x2 = x21%10;
+int x41 = num%100;
+int x4 = x41/10;
+int x5 = num%10;
+if (x1==x5 & x2==x4) 
+{
+    Console.WriteLine($"Число {num} является палиндромом");
+} else 
+{
+    Console.WriteLine($"Число {num} не является палиндромом");
+}
