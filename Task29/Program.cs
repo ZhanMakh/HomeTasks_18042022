a09b39fe0c608a096abd9ba163409b8d24803b60
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-04 00:14:01 +0600

    Добавлено решение задачи № 29

---
+++ Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Домашнее задание. Решение задачи № 29
+int[] massiv = new int[8] {1, 34, 5, 0, 4, 2, 56, 3};
+for(int i=0; i<massiv.Length; i++)
+{
+    Console.Write(massiv[i] + " ");
+}
+Console.WriteLine();
+
+// Второй рандомный способ
+int[] numbers = new int[8];
+for(int i=0; i<numbers.Length; i++)
+{
+    numbers[i]=new Random().Next(2,9);
+    Console.Write(numbers[i] + " ");
+} 
