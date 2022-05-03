ec844400e76b334474f349960ceb7d34056452c6
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-03 22:18:12 +0600

    Добавлено решение задачи № 25

---
+++ Task25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Домашнее задание. Решение задачи № 25
+Console.WriteLine("Введите два числа: ");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int result = 1;
+int i = 0;
+while (i<B)
+{
+    result = result*A;
+    i+=1;
+} 
+Console.WriteLine($"{A} ^ {B} = {result}");
