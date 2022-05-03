9dd6494b5073a2dda521b6910a29c62434505137
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-03 22:43:42 +0600

    Добавлено решение задачи № 27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Домашнее задание. Решение задачи № 27.
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+while (number > 0)
+{
+    int N = number%10;
+    number = number/10;
+    sum = sum + N;
+} Console.WriteLine($"Сумма всех цифр этого числа будет равна {sum}");
