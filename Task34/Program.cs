a2b2dff9622ba6ea35f0e57236fb19027f2dab6f
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-10 00:14:56 +0600

    Добавлено решение задачи № 34

---
+++ Task34/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашнее задание. Решение задачи № 34
+int [] mass = new int[5]; 
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(100,999);
+    Console.Write(mass[i] +" ");
+}
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < mass.Length; i++)
+{
+    if(mass[i]%2==0)
+    {
+        count ++;
+    } continue;
+} Console.WriteLine($"Количество четных чисел равно {count}");
