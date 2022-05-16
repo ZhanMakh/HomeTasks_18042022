d020237491545032bfe9754f7c8930b37b022609
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-16 23:04:05 +0600

    Добавлено решение задачи №52

---
+++ Task52/Program.cs
@@ -0,0 +1,28 @@
+﻿// 52
+int[ , ] massiv = new int[3,4];
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        massiv[i,j] = new Random().Next(1,9);
+    }
+}
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        Console.Write(massiv[i,j] +" ");
+    }Console.WriteLine();
+}
+int sum = 0;
+int min = massiv.GetLength(0);
+
+if(min>massiv.GetLength(1)){
+min=massiv.GetLength(1);
+}
+for (int i = 0; i < min; i++){
+    sum=sum+ massiv[i,i];
+}Console.WriteLine(sum);
+
+
+
