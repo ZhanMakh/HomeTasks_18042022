bd73c3dd60cf5b66e3583c2323ff9c1e178dc6f8
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-16 23:00:53 +0600

    Добавлено решение задачи №41

---
+++ Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Домашнее задание. Решение задачи № 41
+
+int[] numbers = new int[5];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]=new Random().Next(-10,10);
+    Console.Write(numbers[i] + " ");
+} 
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i]>0){
+        count++;
+    }
+} Console.WriteLine($"Количество положительных чисел равно {count}");
