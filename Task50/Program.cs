ddaf29c2523c7e5bac5fb8128f7d0918048ad4fa
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-23 00:08:29 +0600

    Добавлено решение задачи № 50

---
+++ Task50/Program.cs
@@ -0,0 +1,36 @@
+﻿// Домашнее задание № 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+int[ , ] massiv = new int[3,4];
+int[ , ] copy = new int[3,4];
+
+for (int i = 0; i < massiv.GetLength(0); i++)
+{
+    for (int j = 0; j < massiv.GetLength(1); j++)
+    {
+        massiv[i,j] = new Random().Next(1,9);
+        Console.Write(massiv[i,j] +" ");
+    } Console.WriteLine();
+}
+if(num>massiv.Length)
+{
+    Console.Write("Нет такой позиции в массиве");
+} else
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            copy[i,j] = massiv[i,j];
+            count++;
+            if(num==count)
+            {
+             Console.Write(massiv[i,j] + " ");
+                break;
+            }continue; 
+        } 
+    } 
+}
