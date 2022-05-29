2533c858a28e5dc2bfa88a438b321b92d16c21c0
Zhanetta Makhambetova <j_machambet@mail.ru>
2022-05-29 22:51:51 +0600

    Добавлено решение задачи №64

---
+++ Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Домашнее заадние № 64. Задайте значения M и N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+Console.WriteLine("Введите два числа: ");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int length = n - m + 1;
+int[] massiv = new int[length];
+int sum = m;
+Console.Write(m + ", ");
+for (int i = 0; i < massiv.Length; i++)
+    {
+        if(sum<n)
+        {
+            sum = sum + 1;
+            massiv[i] = sum;
+            Console.Write(massiv[i] + ", ");
+        } 
+    } 
